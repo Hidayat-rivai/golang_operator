e7889af4cdc4454d4895d981a3f6ca8c9158cd8a
Hidayat Riva'i <stu.dent.palawija@gmail.com>
2019-12-20 23:48:20 +0700

    operator

---
+++ comparison.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+	var x = 10
+	var z = 5
+	
+	fmt.Println(" == ", x==z)
+	fmt.Println(" != ", x!=z)
+	fmt.Println(" > ", x>z)
+	fmt.Println(" < ", x<z)
+	fmt.Println(" >= ", x>=z)
+	fmt.Println(" <= ", x<=z)
+}
---
+++ logic.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	fmt.Println(" true and true is", true && true)
+	fmt.Println(" true and false is", true && false)
+	fmt.Println(" false and false is", false && false)
+	fmt.Println(" true or true is", true || true)
+	fmt.Println(" true or false is", true || false)
+	fmt.Println(" false or false is", false || false)
+	fmt.Println(" reverse true is", !true)
+	fmt.Println(" reverse false is", !false)
+	
+}
